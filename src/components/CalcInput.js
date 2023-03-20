c3cf7e5fc83c928e36895143882f981f9a63281d
codedit334 <oussama7elabdioui@gmail.com>
2023-03-20 14:59:27 +0000

    Create and style Calculator

---
+++ src/components/CalcInput.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/calcInput.css';
+
+export default function CalcInput() {
+  return (
+    <div className="calcinput">
+      <input className="calcinput-display" placeholder="0" />
+    </div>
+  );
+}
