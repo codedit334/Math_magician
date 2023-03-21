ec69bcd5642c93777f13e32561d91d08d73f495f
codedit334 <oussama7elabdioui@gmail.com>
2023-03-21 12:06:18 +0000

    Add functionality to Calculator

--- src/components/CalcInput.js
+++ src/components/CalcInput.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles/calcInput.css';
 
-export default function CalcInput() {
+export default function CalcInput(props) {
+  const res = { props };
+  //   useEffect(() => console.log(res.props.resObj.total), [res]);
   return (
     <div className="calcinput">
-      <input className="calcinput-display" placeholder="0" />
+      <input className="calcinput-display" value={`${res.props.resObj.next ? res.props.resObj.total + res.props.resObj.operation + res.props.resObj.next : res.props.resObj.total || 0}`} placeholder={res.props.resObj.next || res.props.resObj.total} />
     </div>
   );
 }
