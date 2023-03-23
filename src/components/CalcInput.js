e161d82cb571e914a98ad3fe32376b81e9a81a3a
codedit334 <oussama7elabdioui@gmail.com>
2023-03-23 12:56:19 +0000

    Refactor code

--- src/components/CalcInput.js
+++ src/components/CalcInput.js
@@ -3,10 +3,14 @@ import '../styles/calcInput.css';
 
 export default function CalcInput(props) {
   const res = { props };
-  //   useEffect(() => console.log(res.props.resObj.total), [res]);
   return (
     <div className="calcinput">
-      <input className="calcinput-display" disabled value={`${res.props.resObj.next ? res.props.resObj.total + res.props.resObj.operation + res.props.resObj.next : res.props.resObj.total || 0}`} placeholder={res.props.resObj.next || res.props.resObj.total} />
+      <input
+        className="calcinput-display"
+        disabled
+        value={`${res.props.resObj.total || ''}${res.props.resObj.operation || ''}${res.props.resObj.next || ''}`}
+        placeholder={res.props.resObj.next || res.props.resObj.total}
+      />
     </div>
   );
 }
